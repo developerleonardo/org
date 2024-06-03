75461ea72fae87d85f2d4dfe8558ff58f7e6c9d7
developerleonardo <leo.11hnz@gmail.com>
2024-06-02 21:26:20 -0500

    Responsive design was applied on the project

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,18 +2,20 @@ import './Footer.css';
 
 const Footer = () => {
     return (
-        <footer className="footer" style={{backgroundImage:"url(/img/Footer.webp)"}}>
-        <div className="redes">
-        <a href='https://github.com/developerleonardo'> 
-            <img src="/img/github_logo_icon.png" alt="Github"/>
-        </a>
-        <a href='https://www.linkedin.com/in/leonardo-salazar-serna/'> 
-            <img src="/img/linkedin_icon.png" alt="Linkedin" />
-        </a>
-        </div>
-        <img src='/img/Logo.png' alt='org' /> 
-        <strong>Desarrollado por Leonardo</strong>
-    </footer>
+        <footer className="footer" style={{ backgroundImage: "url(/img/Footer.webp)" }}>
+            <div className="redes">
+                <a href='https://github.com/developerleonardo'>
+                    <img src="/img/github_logo_icon.png" alt="Github" />
+                </a>
+                <a href='https://www.linkedin.com/in/leonardo-salazar-serna/'>
+                    <img src="/img/linkedin_icon.png" alt="Linkedin" />
+                </a>
+            </div>
+            <div className='logo_container'>
+                <img src='/img/Logo.png' alt='org' />
+            </div>
+            <strong>Desarrollado por Leonardo</strong>
+        </footer>
     );
 };
 
